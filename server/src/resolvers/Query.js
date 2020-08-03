d11e26c165b3df34bc393fceec423aa16a5e219a
jason loomis <jason@jrloomis.com>
2020-08-03 03:39:32 -0400

    query created

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,3 +1,24 @@
 module.exports = {
   sanity: () => `>> sane <<`,
+  sort_projects: async (root, args, ctx) => {
+    const where = args.filter
+      ? {
+          OR: [
+            { title: { contains: args.filter } },
+            { description: { contains: args.filter } },
+          ],
+        }
+      : {}
+
+    const projects = await ctx.prisma.project.findMany({
+      where,
+      skip: args.skip,
+      take: args.take,
+      orderBy: args.orderBy,
+    })
+
+    const count = await ctx.prisma.project.count({ where })
+
+    return { projects, count }
+  },
 }
