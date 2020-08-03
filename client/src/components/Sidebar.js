33a0d38483709c5bfdfa9cefb76614e9792e1f13
jason loomis <jason@jrloomis.com>
2020-08-03 08:21:26 -0400

    sidebar setup

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,16 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { navLinks as links } from '../constants/navLinks'
+import { Button } from './Button'
+import logo from '../assets/300.svg' // ! replace
 
 const Sidebar = () => {
   return (
     <header className='sidebar'>
-      <div className='sidebar__logobox'>
-        <h1 className='sidebar__logo'>PLOTr</h1>
-      </div>
-      <nav className='nav'>
-        <a href='example.com'>Link</a>
-        <a href='example.com'>Link</a>
-        <a href='example.com'>Link</a>
-        <a href='example.com'>Link</a>
+      <Link to='/' className='sidebar__logobox'>
+        <img src={logo} alt='Plotr logo' className='sidebar__logo' />
+      </Link>
+      <nav className='sidebar__nav'>
+        {links.map(link => (
+          <Button
+            key={link.id}
+            type={'nav'}
+            path={link.path}
+            text={link.name}
+          />
+        ))}
       </nav>
     </header>
   )
