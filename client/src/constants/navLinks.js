2814a49f2ba73de70b48a8e57275823598b038b1
jason loomis <jason@jrloomis.com>
2020-08-03 08:18:04 -0400

    object of navigation links

---
+++ client/src/constants/navLinks.js
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    id: 1,
+    path: '/about',
+    name: 'Plotter?',
+  },
+  {
+    id: 2,
+    path: '/pricing',
+    name: 'Pricing',
+  },
+  {
+    id: 3,
+    path: '/register',
+    name: 'sign up',
+  },
+  {
+    id: 4,
+    path: '/login',
+    name: 'sign in',
+  },
+]
